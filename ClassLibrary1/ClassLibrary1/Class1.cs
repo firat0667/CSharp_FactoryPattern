16e4a29937917c78bcd55759740c901579f02869
Firat Kocabacak <firat0667@hotmail.com>
2024-01-07 06:42:57 +0300

    Add Employee School  Interface

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HRAdministrationAPI
+{
+   public interface IEmployee
+    {
+        public int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal Salary { get; set; }
+
+    }
+}
